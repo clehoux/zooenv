0ca75634f7f4bcf8a65883b2966f71fa8db53956
Lehoux <Caroline.Lehoux@dfo-mpo.gc.ca>
2023-01-24 10:17:42 -0500

    test4

--- R/query.R
+++ R/query.R
@@ -306,7 +306,7 @@ if(nrow(botresults !=0)){
 
 }
 if(nrow(botresults) ==0) botresults2 <-  data.frame(ID=ID[i], CPHL=NA)
-stop("all query completed")
+
 prof<- suppressMessages(list(xbresults2, results2, botresults2) %>%  reduce(dplyr::full_join))
 
 if(!is.null(depth.max)) prof$depth.max = depth.max[i]
@@ -314,6 +314,7 @@ if(is.null(depth.max)) prof$depth.max =max(prof$PROFD_MAX_JD)
 
 if(ncol(results2) >3 | ncol(xbresults2)>3){
 ctd<- summarize_CTD(data=prof,depth_range=range_ctd)
+stop("summarizeCTD completed")
 }
 if(ncol(botresults2) >3){
 bot <-  summarize_BOT_STRAT(data=prof, depth_range=range_bot)

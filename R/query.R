c61646a1c382e9caed6908b453bae34b2e031bf2
Lehoux <Caroline.Lehoux@dfo-mpo.gc.ca>
2023-02-02 11:23:30 -0500

    round direct

--- R/query.R
+++ R/query.R
@@ -307,7 +307,9 @@ if(nrow(botresults !=0)){
 }
 if(nrow(botresults) ==0) botresults2 <-  data.frame(ID=ID[i], CPHL=NA)
 
-prof<- suppressMessages(list(xbresults2, results2, botresults2) %>%  purrr::reduce(dplyr::full_join) %>%  mutate(DEPH=round(DEPH)))
+prof<- suppressMessages(list(xbresults2, results2, botresults2) %>%  purrr::reduce(dplyr::full_join))
+prof$DEPH <-  round(prof$DEPH)
+
 
 if(!is.null(depth.max)) prof$depth.max = depth.max[i]
 if(is.null(depth.max)) prof$depth.max =max(prof$PROFD_MAX_JD)
